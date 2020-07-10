5574b90e3d922b8c1f369844b0dfd24b57765ae1
flowMemory <772675479@qq.com>
2020-07-10 18:36:53 +0800

    学习vuex基本知识1

--- base.js
+++ base.js
@@ -2,12 +2,17 @@
     vue 项目学习：
         项目类型：SaaS
         技术预计构成：
-            vue, vue-router, vuex, vue-cli, 
-            iconfont, echart, swiper, 3D-threejs, 地图map, tween
-            css3动画
-            UI库 - elementUI or antdesign
-            git管理
-            ...
+            一期：
+                vue, vue-router, vuex, vue-cli, vue3.X
+                iconfont, echart, swiper, 3D-threejs, 地图map, tween
+                css3动画
+                UI库 - elementUI or antdesign
+                git管理...
+
+            二期：
+                node.js 引入服务端
+
+
 
 
 
